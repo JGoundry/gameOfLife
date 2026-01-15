2ff12755f7186e7da4c49b608abb9eeda7533718
Josh Goundry <joshuagoundry@gmail.com>
2026-01-15 00:17:30 +0000

    Refactor.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,46 @@
-#include "gameOfLife.hpp"
-
 #include <exception>
 #include <iostream>
 #include <random>
 #include <string>
 
+#include "gameOfLife.hpp"
+
 namespace {
 
 Grid generateGrid(size_t rows, size_t cols) {
-    std::random_device rd;
-    std::mt19937 gen(rd());
+  std::random_device rd;
+  std::mt19937 gen(rd());
 
-    std::bernoulli_distribution d(0.5);
+  std::bernoulli_distribution d(0.5);
 
-    Grid grid{rows, std::vector<bool>(cols)};
-    for (auto& row : grid)
-    {
-        for (auto cell : row) {
-            cell = d(gen);
-        }
+  Grid grid{rows, Grid::value_type(cols)};
+  for (auto& row : grid) {
+    for (auto cell : row) {
+      cell = d(gen);
     }
-    return grid;
+  }
+  return grid;
 }
 
-}
+}  // namespace
 
 int main(int argc, char* argv[]) {
-    if (argc != 4) {
-        std::println(std::cerr, "Usage: {} <rows> <cols> <iterations>", argv[0]);
-        return 1;
-    }
+  if (argc != 4) {
+    std::println(std::cerr, "Usage: {} <rows> <cols> <iterations>", argv[0]);
+    return 1;
+  }
 
-    size_t rows{}, cols{}, iterations{};
-    try {
+  size_t rows{}, cols{}, iterations{};
+  try {
     rows = std::stoull(argv[1]);
     cols = std::stoull(argv[2]);
     iterations = std::stoull(argv[3]);
-    } catch ( const std::exception& e ) {
-        std::println(std::cerr, "Caught '{}' trying to convert args to nums.", e.what());
-    }
+  } catch (const std::exception& e) {
+    std::println(std::cerr, "Caught '{}' trying to convert args to nums.",
+                 e.what());
+  }
 
+  gameOfLife(generateGrid(rows, cols), iterations);
 
-    gameOfLife(generateGrid(rows, cols), iterations);
-
-    return 0;
+  return 0;
 }
